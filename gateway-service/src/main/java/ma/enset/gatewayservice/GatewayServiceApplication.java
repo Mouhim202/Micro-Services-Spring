9b7699e0e3eda303243ee53775af04f5010aa762
unknown <meryemmouhim123@gmail.com>
2025-12-07 12:56:13 +0100

    Configuration dynamique des routes de la gateway

--- gateway-service/src/main/java/ma/enset/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/ma/enset/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package ma.enset.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +18,15 @@ public class GatewayServiceApplication {
     }
 
 //    @Bean
-//    public RouteLocator routes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("r1", r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
-//                .route("r2", r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
-//                .build();
-//    }
+  public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+               .route("r1", r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+               .route("r2", r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+                .build();
+   }
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
 
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
 }
